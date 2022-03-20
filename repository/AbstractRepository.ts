8cb388083ef1a78d9ded59b5d458724b04825801
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2022-03-20 14:16:21 +0300

    repository abstract removed

--- repository/AbstractRepository.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractAggregateRoot } from '../aggretages/AbstractAggregateRoot'
-
-export abstract class AbstractRepository {
-    abstract save(aggregate: AbstractAggregateRoot): boolean
-
-    abstract getById(aggregateId: number): AbstractAggregateRoot | null
-}
