d704ed8f4cfc08b57ebdd75b7580d0600e9a164f
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2022-04-02 18:19:36 +0300

    v1.0.21

--- events/Topics.ts
+++ events/Topics.ts
@@ -1,5 +1,20 @@
-export enum Topics {
-    Tweets = 'tweets-topic',
-    Users = 'users-topic',
-    Comments = 'users-comments',
+enum Tweets {
+    create = 'tweets-create-topic',
+    update = 'tweets-update-topic',
+}
+
+enum Users {
+    create = 'users-create-topic',
+    update = 'users-update-topic',
+}
+
+enum Comments {
+    create = 'comments-create-topic',
+    update = 'comments-update-topic',
+}
+
+export const Topics = {
+    Tweets,
+    Users,
+    Comments,
 }
